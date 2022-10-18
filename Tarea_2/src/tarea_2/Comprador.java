088c3262cec13689d5edde592c8acf2b7a48d33a
Keyteer <pedro.p0020@gmail.com>
2022-10-18 11:57:55 -0300

    cliente a medio hacer maomeno haci noma

--- Tarea_2/src/tarea_2/Comprador.java
+++ Tarea_2/src/tarea_2/Comprador.java
@@ -2,14 +2,18 @@ package tarea_2;
 
 public class Comprador {
 
-    Moneda m;
-    Expendedor exp;
-    int numBebida, vuelto;
+    private Moneda m;
+    private Expendedor exp;
+    private int numBebida, vuelto;
+    private String sabor;
 
     public Comprador(Moneda m, Expendedor exp, int numBebida) {
         this.m = m;
         this.exp = exp;
         this.numBebida = numBebida;
         this.vuelto = 0;
+        this.sabor = "";
     }
+
+    public
 }
