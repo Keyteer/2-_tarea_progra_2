ee7b8ffc3e947e5d9af44fc44a92a050ce9d0b37
Keyteer <>
2022-10-26 15:03:36 -0300

    cambios en comprador y getter de sabor

--- Tarea_2/src/tarea_2/Comprador.java
+++ Tarea_2/src/tarea_2/Comprador.java
@@ -2,29 +2,36 @@ package tarea_2;
 
 public class Comprador {
 
-    private Moneda m;
-    private Expendedor exp;
-    private int numBebida, vuelto;
-    private String sabor;
+	private Moneda m;
+	private Expendedor exp;
+	private int numBebida, vuelto;
+	private String sabor;
 
-    public Comprador(Moneda m, Expendedor exp, int numBebida) {
-        this.m = m;
-        this.exp = exp;
-        this.numBebida = numBebida;
-        vuelto = 0;
-        sabor = "";
-    }
+	public Comprador(Moneda m, Expendedor exp, int numBebida) {
+		this.m = m;
+		this.exp = exp;
+		this.numBebida = numBebida;
+		vuelto = 0;
+		sabor = "";
+	}
 
-    /*public void comprar(){
-        this.beber(exp.comprarBebida(m,numBebida));
-        
-        while (m != null){
-            m = exp.getVuelto();
-            vuelto += m.getValor();
-        }
-    }*/
-    
-    public void beber(Bebida b){
-        sabor = b.tomarBebida();
-    }
+	public void comprar() {
+		this.beber(exp.comprarBebida(m, numBebida));
+
+		while (true) {
+			m = exp.getVuelto();
+			if (m == null)
+				break;
+
+			vuelto += m.getValor();
+		}
+	}
+
+	public void beber(Bebida b) {
+		sabor = b.tomarBebida();
+	}
+
+	public String getSabor(){
+		return sabor;
+	}
 }
